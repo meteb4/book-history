002f8cda9ae9b89e20ca9ffa804d2bb14eb86156
meteb <mahmoudjean@gmail.comgit config --global --listgit config --global user.email mahmoudjean@gmail.com>
2022-06-05 18:10:33 +0200

    premiere version de la base de données terminée

--- database/database.js
+++ database/database.js
@@ -17,27 +17,22 @@ class Database {
     this.database.end(() => console.log("DATABASE DISCONNECTED"));
   }
 
-  add(name) {
+  add(title, author) {
     this.database.query(
-      `INSERT INTO books (title) VALUES ("${name}")`,
-      (err, result) => {
+      `INSERT INTO books (title, author) VALUES ("${title}","${author}")`,
+      (err, res) => {
         if (err) throw err;
+        console.log(res);
       }
     );
   }
-  search = {
-    name: (name) =>
-      this.database.query(
-        `SELECT * FROM books WHERE title="${name}"`,
-        (err, res) => {
-          if (err) throw err;
-          console.log(JSON.stringify(res[0]));
-        }
-      ),
-  };
+
+  delete(id) {
+    this.database.query(`DELETE FROM books WHERE id = ${id}`, (err, res) => {
+      if (err) throw err;
+      console.log(res);
+    });
+  }
 }
 
-let i = new Database("localhost", "root", "", "book");
-i.connect;
-i.search.name("hi");
-i.end;
+module.exports = Database;
