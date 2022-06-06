02c023cecea39318628bfc42febbafd9dca4e477
ieatchild <64801684+ieatchild@users.noreply.github.com>
2022-06-06 22:17:32 +0200

    cant access http://localhost:3000/books/add using post request (from web browser

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const DATABASE = new Database("localhost", "root", "", "book");
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Access-Headers", "Content-Type");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    "GET, POST, PUT, PATCH, DELETE"
   );
   next();
 });
@@ -19,6 +20,14 @@ app.get("/", (req, res) => {});
 app.get("/books", async (req, res) => {
   const books = await DATABASE.show.then((e) => e);
   res.send(books);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post("/books/add", (req, res) => {
+  console.log(JSON.parse(req.body));
+  DATABASE.add(req.body.title, req.body.author, req.body.available);
 });
 
 app.listen(3000);
